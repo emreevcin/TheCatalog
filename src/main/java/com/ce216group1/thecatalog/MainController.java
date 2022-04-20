b46bce65557ad1a49525b4270a9b7df97f3efa89
Emre Evcin <emreevcin@icloud.com>
2022-04-20 17:59:43 +0300

    GUI build steps 3
    2 scenes are created.

--- src/main/java/com/ce216group1/thecatalog/MainController.java
+++ src/main/java/com/ce216group1/thecatalog/MainController.java
@@ -2,15 +2,13 @@ package com.ce216group1.thecatalog;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.TreeTableView;
 
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class MainController implements Initializable {
 
     @FXML
     private TreeTableView<?> treeTableView;
